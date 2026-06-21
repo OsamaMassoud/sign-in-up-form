ca78606e18f47ed5a6d98f601cbfab9cd6094bb6
osama <o.tawakul@gmail.com>
2026-06-21 19:44:17 +0100

    Fix GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,16 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/sign-in-up-form/', 
   plugins: [react(), tailwindcss()],
-  define: {
-    'import.meta.env.VITE_BACKEND_URL':JSON.stringify(process.env.VITE_BACKEND_URL)
-  },
-  server: {
-    proxy: {
-      '/api': {
-        target: process.env.VITE_BACKEND_URL || 'http://localhost:8500',
-        changeOrigin: true
-      }
-    }
-  }
 })
